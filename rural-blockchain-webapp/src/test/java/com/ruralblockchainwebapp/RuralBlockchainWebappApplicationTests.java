db036ede797ada8b6c8af79cc0479294d41aaba3
Nke Yapo <yaposebastien@gmail.com>
2019-06-13 07:44:18 -0400

    Add front-end folder

---
+++ rural-blockchain-webapp/src/test/java/com/ruralblockchainwebapp/RuralBlockchainWebappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ruralblockchainwebapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RuralBlockchainWebappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
