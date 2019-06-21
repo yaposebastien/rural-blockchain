0d5f0d11f4b1beeb5204678e67c50c8e0ccae0bb
Nke Yapo <yaposebastien@gmail.com>
2019-06-21 14:19:44 -0400

    Modified smart contract

--- contracts/RuralContract.sol
+++ contracts/RuralContract.sol
@@ -4,27 +4,23 @@ contract RuralContract {
 
    //Defining the model of the LandTitle
    struct LandTitle {
-       string _code;
-       string _ownerFirstName;
-       string _ownerLastName;
-       uint256 _latLand;
-       uint256 _longLand;
-       uint256 _areaLand;
-       string _city;
-       string _villageName;
+       uint id;
+       string firstName;
+       string lastName;
+       int256 geoLatLand;
+       int256 geoLongLand;
+       uint256 areaLand;
+       string city;
+       string villageName;
    }
 
    //Define variables of the contract
    LandTitle[] public landTitles;
-   uint256 public totalOfLandTitles;
-   address[16] public registrationAccount;
-   
-    //mapping(uint => LandTitle) public landTitles;
+   uint public nextId = 1;
+
     
     //Declare Constructor
     constructor() public {
-       totalOfLandTitles = 0;
-       
     }
 
     //Declare List of the Functions of the contract
@@ -32,141 +28,106 @@ contract RuralContract {
     /*
     Event LandTitleEvent to help use the logging facility to create a new landTitle
     */
-    event LandTitleRegistrationEvent(string _code, string _ownerFirstName,
-                        string _ownerLastName, uint256 _latLand,
-                        uint256 _longLand, uint256 _areaLand,
-                        string _city, string _villageName
+    event LandTitleRegistrationEvent(uint id, string firstName,
+                        string lastName, int256 geoLatLand,
+                        int256 geoLongLand, uint256 areaLand,
+                        string city, string villageName
     );
     
     /*
     Event DeleteLandTitleEvent to help use the logging facility to delete a landTitle
     */
-    event DeleteLandTitleEvent(string _code);
+    event DeleteLandTitleEvent(uint id);
 
     /*
     Event UpdateLandTitleEvent to help use the logging facility to update a landTitle
     */
-    event UpdatedLandTitleOwnerEvent(string _code,string _ownerFirstName,
-                               string _ownerLastName
+    event UpdatedLandTitleOwnerEvent(uint id, string firstName,
+                               string lastName
 
     );
     
     /*
     Function registerLandTitle
-    @Param _code
-    @Param _ownerFirstName,
-    @Param _ownerLastName,
-    @Param _latLand,
-    @Param _longLand,
-    @Param _areaLand, 
-    @Param _city,
-    @Param _villageName
-    
-    @Return totalOfLandTitles
+    @Param firstName,
+    @Param lastName,
+    @Param geoLatLand,
+    @Param geoLongLand,
+    @Param areaLand, 
+    @Param city,
+    @Param villageName
     */
 
     //Register your land
     function registerLandTitle(
-        string memory _code,
-        string memory _ownerFirstName,
-        string memory _ownerLastName,
-        uint256  _latLand,
-        uint256 _longLand,
-        uint256 _areaLand, 
-        string memory _city,
-        string memory _villageName
-        ) 
-         public returns (uint256 totalOfLandTitles ) {  
-             LandTitle memory newLandTitle =  LandTitle(_code,
-                        _ownerFirstName, _ownerLastName,
-                        _latLand, _longLand, _areaLand,
-                        _city, _villageName 
-                        );
-                
-            landTitles.push(newLandTitle);
-            totalOfLandTitles++;
-            emit LandTitleRegistrationEvent(_code,_ownerFirstName,
-                                _ownerLastName, _latLand,
-                                _longLand, _areaLand,
-                                _city, _villageName
-                                );
-            return totalOfLandTitles;
-
+        string memory firstName,
+        string memory lastName,
+        int256  geoLatLand,
+        int256  geoLongLand,
+        uint256 areaLand, 
+        string memory city,
+        string memory villageName
+        ) public { 
+            landTitles.push(LandTitle(nextId,firstName,lastName,geoLatLand,geoLongLand,areaLand,city,villageName));
+            nextId++;
     } 
 
     /*
     Function ModifyLandTitleOwner
-    @Param newCode
-    @Param newOwnerFirstName
-    @Param newOwnerLastName
+    @Param id
+    @Param newFirstName
+    @Param newLastName
     */
-    function UpdatedLandTitleOwner(string memory newCode, string memory newOwnerFirstName, string memory newOwnerLastName) public returns(bool) {
-
-        for(uint256 i=0; i<totalOfLandTitles; i++) {
-            if(compareStrings(landTitles[i]._code, newCode)) {
-                landTitles[i]._ownerFirstName = newOwnerFirstName;
-                landTitles[i]._ownerLastName = newOwnerLastName;
-
-                emit UpdatedLandTitleOwnerEvent(newCode, newOwnerFirstName, newOwnerLastName);
-                return true;
-            }  
-        }
-        return false;
+    function UpdatedLandTitleOwner(uint id, string memory newFirstName, string memory newLastName) public {
+        uint i = search(id);
+        landTitles[i].firstName = newFirstName;
+        landTitles[i].lastName = newLastName;
+        
+        emit UpdatedLandTitleOwnerEvent(id, newFirstName, newLastName);
     }
 
     /*
     Function DeleteLandTitle to delete a land title
     */
-    function DeleteLandTitle(string memory _codeDel) public returns (bool isDeleted) {
-        require(totalOfLandTitles > 0);
+    function DeleteLandTitle(uint id) public {
+        uint i = search(id);
+        delete landTitles[i];
+        
+        emit DeleteLandTitleEvent(id);
 
-        for(uint256 index=0; index< totalOfLandTitles; index++) {
-            if(compareStrings(landTitles[index]._code, _codeDel)) {
-                landTitles[index] = landTitles[totalOfLandTitles - 1];
-                delete landTitles[totalOfLandTitles - 1];
-                totalOfLandTitles--;
-                landTitles.length--;
-
-                emit DeleteLandTitleEvent(_codeDel);
-                return true;
-            }
-        }
-        return false;
     }
 
      //Retrieve a specific Land Title saved
-    function getLandTitle(string memory _codeRequested) public view returns(
-                        string memory _code, string memory _ownerFirstName,
-                        string memory _ownerLastName, uint256  _latLand,
-                        uint256  _longLand, uint256  _areaLand, 
-                        string memory _city, string memory _villageName){
-
-            for(uint256 i = 0; i < totalOfLandTitles; i++) {
-                if(compareStrings(landTitles[i]._code, _codeRequested)) {
-                    
-                    return (
-                        landTitles[i]._code, landTitles[i]._ownerFirstName,
-                        landTitles[i]._ownerLastName, landTitles[i]._latLand,
-                        landTitles[i]._longLand, landTitles[i]._areaLand,
-                        landTitles[i]._city, landTitles[i]._villageName
-                    );
-                }
-            }
-            revert('This country code was not found');
-    } 
-
-
-    /*
-    Function compareStrings to compare the code of the land title given as reference
-    */
-    function compareStrings (string memory codeA, string memory codeB) internal pure returns(bool) {
-        return keccak256(abi.encodePacked(codeA)) == keccak256(abi.encodePacked(codeB));
-
+    function getLandTitle(uint id) view public returns(uint, string memory,
+                                    string memory, int256,
+                                    int256, uint256,string memory, string memory ){
+        uint i = search(id);
+        return (landTitles[i].id , landTitles[i].firstName, landTitles[i].lastName,
+                landTitles[i].geoLatLand, landTitles[i].geoLongLand, landTitles[i].areaLand,
+                landTitles[i].city, landTitles[i].villageName );
     }
 
-    //Function to return the number of LandTitles Saved
-    function getTotalOfLandTitles() public view returns (uint256 length) {
-        return landTitles.length;
+    function search(uint id) view internal returns(uint) {
+        for(uint i=0; i < landTitles.length; i++) {
+            if(landTitles[i].id == id) {
+                return i;
+            }
+        }
+        revert("Land Title ID does not exist");
+    }
+
+   
+
+    //Function to return all LandTitles Saved
+    function getAllOwners() public view returns (uint, string memory,
+                                    string memory, int256, int256, uint256,
+                                    string memory, string memory) {
+        
+        for(uint i=1; i < landTitles.length; i++) {
+            return (landTitles[i].id , landTitles[i].firstName, landTitles[i].lastName,
+                landTitles[i].geoLatLand, landTitles[i].geoLongLand, landTitles[i].areaLand,
+                landTitles[i].city, landTitles[i].villageName );    
+        }
     }
 
 }
